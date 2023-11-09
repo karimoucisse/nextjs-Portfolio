c3c06d4dbd9e1c6a9ca7ea9e2f1f677434bc0390
karimou cisse <karimou@pylote.io>
2023-11-09 17:05:58 +0100

    add: navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] 
           lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
         ></div>
+        <Header />
         {children}
       </body>
     </html>
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,12 @@
-const header = () => {
-  return <div>header</div>;
-};
-
-export default header;
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+      <div
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border
+       border-white border-opacity-40 bg-white bg-opacity-80
+       shadow shadow-black/[0.03] backdrop:-blur-[0.rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem]
+       sm:rounded-full"
+      ></div>
+    </header>
+  );
+}
