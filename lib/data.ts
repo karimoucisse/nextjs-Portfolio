82357ec553c926226a2e8e5830a216b5e84f6659
karimou cisse <karimou@pylote.io>
2023-11-09 16:46:02 +0100

    add: folder component, add file header.tsx in component folder

--- lib/data.ts
+++ lib/data.ts
@@ -8,7 +8,7 @@ import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
-    name: "Home",
+    name: "Acueil",
     hash: "#home",
   },
   {
@@ -20,11 +20,11 @@ export const links = [
     hash: "#projects",
   },
   {
-    name: "Skills",
+    name: "Compétences",
     hash: "#skills",
   },
   {
-    name: "Experience",
+    name: "Experiences",
     hash: "#experience",
   },
   {
@@ -35,28 +35,20 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Alternance Développeur Full Stack",
+    location: "Station F",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2022 - 2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Stage Data scientist",
+    location: "Paris",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2022",
   },
 ] as const;
 
@@ -89,19 +81,19 @@ export const skillsData = [
   "CSS",
   "JavaScript",
   "TypeScript",
-  "React",
+  "React.js",
   "Next.js",
   "Node.js",
+  "Express.js",
   "Git",
   "Tailwind",
-  "Prisma",
+  "Material UI",
   "MongoDB",
+  "DynamoDB",
+  "Airtable",
   "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
   "Python",
-  "Django",
-  "Framer Motion",
+  "Php",
+  "Power BI",
+  "Oracle APEX",
 ] as const;
